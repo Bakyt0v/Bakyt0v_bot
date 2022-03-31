3afce2484ff27242dbe6a4b05af38120671312c1
Bakyt0v <Amantur470@gmail.com>
2022-03-31 19:27:31 +0600

    webhook

--- config_bot.py
+++ config_bot.py
@@ -8,3 +8,4 @@ storage = MemoryStorage()
 TOKEN = config("TOKEN")
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
+URL = "https://bakytov.herokuapp.com/"
--- tg_bot.py
+++ tg_bot.py
@@ -1,13 +1,15 @@
 import asyncio
 from aiogram import executor
-from config_bot import dp
+from config_bot import dp, bot,URL
 from handler import callback, cilent, extra, notification, inline
 from fsm_admin import fsmadmin, fsm_admin_users
 from database import bot_db, user_db
 from handler.notification import scheduler, deadline_hw
+from decouple import config
 
 
 async def on_startup(_):
+    await bot.set_webhook(URL)
     bot_db.sql_create()
     user_db.sql_create()
     asyncio.create_task(scheduler())
@@ -15,8 +17,8 @@ async def on_startup(_):
 
     print('Bot is online')
 
-
-
+async def on_shutdown(dp):
+    await  bot.delete_webhook()
 
 
 fsm_admin_users.register_handler_fsm_admin_user(dp)
@@ -29,3 +31,13 @@ notification.register_handler_notification(dp)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path="",
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host="0.0.0.0",
+        port=int(config("PORT", default=5000)),
+
+             )
